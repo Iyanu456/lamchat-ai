1c38a3d0e933fe357706d4da0ad5d24398448a2c
Iyanu456 <oyerindei13@gmail.com>
2023-09-03 08:26:54 +0100

    new changes to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,17 @@ export default function Home() {
                     <button>See More</button>
                 </div>
             </section>
+            <section className="main-section mt-20">
+                <h2>Introducing LamChat</h2>
+                <div>LamChat is your intelligent chat companion, designed to engage, inform, and entertain. Whether you're seeking answers, looking for interesting discussions, or just want to chat, LamChat is here to make your conversations enjoyable and informative.</div>
+            </section>
+            <section className="main-section">
+                <h2>Why LamChat</h2>
+                <div><b>Great Conversations:</b> LamChat is here for fun and informative chats. It's really good at making conversations interesting.</div>
+                <div className="mt-10"><b>Quick Answers:</b> No more waiting! LamChat gives you fast responses, so you don't have to wait long.</div>
+                <div className="mt-10"><b>Tailored for You:</b> LamChat talks to you in a way that you like. It's like having a friend who knows what you like to talk about.</div>
+            </section>
+            
         </div>
     )
 }
