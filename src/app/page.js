3c21ebf1e9be715d69b34d9c2893693125b9458b
Iyanu456 <oyerindei13@gmail.com>
2023-09-05 01:13:41 +0100

    resolved package.json conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,8 @@ export default function Home() {
         <div>
             <NavBar />
             <section className="heading-section">
-                <h1>Welcome to <span className="gradient">LamChat</span> Where Conversations come to Life</h1>
-                <p className="mt-20">Welcome to LamChat, where the power of AI-driven conversations meets your curiosity and interests.</p>
-                <div className="flex g-10 mt-35">
+                <h1>Welcome to <b>LamChat ai</b> Where Conversations come to Life</h1>
+                <div className="flex g-10 mt-40">
                     <button>Get Started</button>
                     <button>See More</button>
                 </div>
@@ -25,35 +24,35 @@ export default function Home() {
             <main className="main-group">
                 <IntroSection />
                 <section className="how-section">
-                    <h2 style={largeHeading}>How Does LamChat Work?</h2>
+                    <h2 style={largeHeading} className="mb-5">How Does LamChat Work?</h2>
                     <div>
                         LamChat is powered by state-of-the-art AI technology. It understands your questions and provides relevant, accurate information in real-time. Our ever-evolving chatbot adapts to your needs, ensuring each interaction is meaningful and productive.
                     </div>
                 </section>
                 <section className="samples-section">
-                    <h2 style={largeHeading}>Samples</h2>
+                    <h2 style={largeHeading} className="mb-5">Samples</h2>
                     <div>
                         LamChat is powered by state-of-the-art AI technology. It understands your questions and provides relevant, accurate information in real-time. Our ever-evolving chatbot adapts to your needs, ensuring each interaction is meaningful and productive.
                     </div>
                 </section>
                 <section className="why-section mb-30">
-                    <h2 style={largeHeading}>Why LamChat?</h2>
+                    <h2 style={largeHeading} className="mb-5">Why LamChat?</h2>
                     <div>
-                        <div className="card" style={firstCard}>
+                        <div className="card" >
                             <h1>Great Conversations</h1>
                             <div>
                                 <div className="line"></div>
                                 <div className="content">{"LamChat is here for fun and informative chats. It's really good at making conversations interesting"}.</div>
                             </div>
                         </div>
-                        <div className="card" style={firstCard}>
+                        <div className="card" >
                             <h1>Great Conversations</h1>
                             <div>
                                 <div className="line"></div>
                                 <div className="content">{"LamChat is here for fun and informative chats. It's really good at making conversations interesting."}</div>
                             </div>
                         </div>
-                        <div className="card" style={firstCard}>
+                        <div className="card" >
                             <h1>Great Conversations</h1>
                             <div>
                                 <div className="line"></div>
